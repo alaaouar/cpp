f9564d8c7a1143e29ce174698a5b535f759353d0
alaaouar <alaaouar@student.1337.ma>
2025-11-17 02:49:32 +0100

    [done] cpp09

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: alaaouar <alaaouar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 00:46:05 by alaaouar          #+#    #+#             */
-/*   Updated: 2025/11/17 01:37:16 by alaaouar         ###   ########.fr       */
+/*   Updated: 2025/11/17 02:48:38 by alaaouar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,38 @@ PmergeMe::~PmergeMe()
 {
 }
 
+void vect_Inserting(std::vector<int>& cont, int value, size_t end)
+{
+    if (cont.empty()) 
+    {
+        cont.push_back(value);
+        return;
+    }
+    size_t left = 0;
+    size_t right = end;
+    while(left <= right)
+    {
+        size_t mid = (left + right) / 2;
+        if(cont[mid] == value)
+        {
+            cont.insert(cont.begin() + mid, value);
+            return;
+        }
+        else if(cont[mid] < value)
+            left = mid + 1;
+        else
+        {	
+            if(mid == 0)
+            {
+                left = 0;
+                break;
+            }
+            right = mid - 1;
+        }
+    }
+    cont.insert(cont.begin() + left, value);
+}
+
 void PmergeMe :: parsing(int ac, char **av)
 {
     if(ac < 2)
@@ -55,6 +87,11 @@ void PmergeMe :: parsing(int ac, char **av)
     }
 }
 
+static bool secomp(const std::pair<int, int>& a, const std::pair<int, int>& b) 
+{
+        return a.second < b.second;
+}
+
 std::vector<size_t> PmergeMe::jacob_algo(size_t n)
 {
     std::vector<size_t> ject;
@@ -74,9 +111,87 @@ std::vector<size_t> PmergeMe::jacob_algo(size_t n)
     return ject;
 }
 
-static bool secomp(const std::pair<int, int>& a, const std::pair<int, int>& b) 
+void deq_Inserting(std::deque<int>& cont, int value, size_t end)
 {
-        return a.second < b.second;
+    if (cont.empty()) 
+    {
+        cont.push_back(value);
+        return;
+    }
+    size_t left = 0;
+    size_t right = end;
+    while(left <= right)
+    {
+        size_t mid = (left + right) / 2;
+        if(cont[mid] == value)
+        {
+            cont.insert(cont.begin() + mid, value);
+            return;
+        }
+        else if(cont[mid] < value)
+            left = mid + 1;
+        else
+        {	
+            if(mid == 0)
+            {
+                left = 0;
+                break;
+            }
+            right = mid - 1;
+        }
+    }
+    cont.insert(cont.begin() + left, value);
+}
+
+std::deque<int> PmergeMe::deque_sort(std::deque<int>& data)
+{
+    std::deque<int> bigs;
+    std::deque<int> smalls;
+    std::deque<std::pair<int, int> > pairs;
+    int rest; 
+    size_t i = 0;
+    for (; i < data.size(); i += 2)
+    {
+        if(i + 1 < data.size())
+        {    
+            if(data[i] < data[i + 1])
+                pairs.push_back(std::make_pair(data[i], data[i + 1]));
+            else
+                pairs.push_back(std::make_pair(data[i + 1], data[i]));
+        }
+    }
+    std::sort(pairs.begin(), pairs.end(), secomp);
+    bool hasRest = false;
+    if(data.size() % 2 != 0)
+    {
+        rest = data.back();
+        hasRest = true;
+    }
+    for (size_t j = 0; j < pairs.size(); j++)
+    {
+        bigs.push_back(pairs[j].second);
+        smalls.push_back(pairs[j].first);
+    }
+    std::vector<size_t> order = jacob_algo(smalls.size());
+    std::vector<bool> inserted(smalls.size(), false);
+    for(size_t index = 0; index < order.size(); index++)
+    {
+        size_t pos = order[index];
+        if(pos < smalls.size() && !inserted[pos])
+        {
+            int val = smalls[pos];
+            deq_Inserting(bigs, val, bigs.size() - 1);
+            inserted[pos] = true;
+        }
+    }
+    for (size_t k = 0; k < smalls.size(); k++)
+    {
+        if (!inserted[k])
+            deq_Inserting(bigs, smalls[k], bigs.size() - 1);
+    }
+    if(hasRest)
+        deq_Inserting(bigs, rest, bigs.size() - 1);
+    return bigs;
 }
 
 std::vector<int> PmergeMe :: vector_sort(std::vector<int> &vectt)
@@ -111,10 +226,13 @@ std::vector<int> PmergeMe :: vector_sort(std::vector<int> &vectt)
         size_t pos = order[k];
         if(pos < smalls.size() && !inserted[pos])
         {
-            Inserting(bigs, smalls[pos], bigs.size() - 1);
+            vect_Inserting(bigs, smalls[pos], bigs.size() - 1);
             inserted[pos] = true;
         }
     }
+    if (hasRest)
+        vect_Inserting(bigs, rest, bigs.size() - 1);
+    return bigs;
 }
 
 void PmergeMe :: run (int ac, char **av)
@@ -126,4 +244,18 @@ void PmergeMe :: run (int ac, char **av)
     std::cout << std::endl;
     std::clock_t startime = std::clock();
     std::vector<int> sortedVec = vector_sort(vect);
+    std::clock_t endVec = std::clock();
+    double vectorTime = static_cast<double>(endVec - startime) / CLOCKS_PER_SEC * 1e6;
+    std::clock_t startDeq = std::clock();
+    std::deque<int> sortedDeq = deque_sort(deq);
+    std::clock_t endDeq = std::clock();
+    double dequeTime = static_cast<double>(endDeq - startDeq) / CLOCKS_PER_SEC * 1e6;
+    vect = sortedVec;
+    deq = sortedDeq;
+    std::cout << "After:  ";
+    for (size_t i = 0; i < vect.size(); i++)
+        std::cout << vect[i] << " ";
+    std::cout << std::endl;
+    std::cout << "Time to process a range of " << vect.size() << " elements with std::vector : " << std::fixed << std::setprecision(5) << vectorTime << " us" << std::endl;
+    std::cout << "Time to process a range of " << deq.size() << " elements with std::deque : " << std::fixed << std::setprecision(5) << dequeTime << " us" << std::endl;
 }
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: alaaouar <alaaouar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 00:44:46 by alaaouar          #+#    #+#             */
-/*   Updated: 2025/11/17 01:41:09 by alaaouar         ###   ########.fr       */
+/*   Updated: 2025/11/17 02:45:49 by alaaouar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@
 #include <algorithm>
 #include <climits>
 #include <ctime>
+#include <iomanip>
 
 class PmergeMe
 {
@@ -27,39 +28,8 @@ private:
     std::vector<int> vect;
     std::deque<int> deq;
     std::vector<int> vector_sort(std::vector<int> &vectt); 
+	std::deque<int> deque_sort(std::deque<int>& data);
     std::vector<size_t> jacob_algo(size_t n);
-    template <typename Container>
-		void Inserting(Container& cont, int value, size_t end)
-		{
-			 if (cont.empty()) 
-    		{
-       			cont.push_back(value);
-        		return;
-    		}
-			size_t left = 0;
-			size_t right = end;
-			while(left <= right)
-			{
-				size_t mid = (left + right) / 2;
-				if(cont[mid] == value)
-				{
-					cont.insert(cont.begin() + mid, value);
-					return;
-				}
-				else if(cont[mid] < value)
-					left = mid + 1;
-				else
-				{	
-					if(mid == 0)
-					{
-						left = 0;
-						break;
-					}
-					right = mid - 1;
-				}
-			}
-			cont.insert(cont.begin() + left, value);
-		};
 public:
     PmergeMe();
     PmergeMe(const PmergeMe& other);
